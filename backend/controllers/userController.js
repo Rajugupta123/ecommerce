ffa3e784b38b2c902fc05f77ef3f45e45ed9b848
Rajugupta123 <raju.kgupta77@gmail.com>
2023-03-27 23:18:43 +0545

    user password encrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,3 +3,21 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 
 //Register A User.
+exports.registerUser = catchAsyncErrors(async(req,res,next)=>{
+    const {name,email,password} = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar:{
+            public_id:"this is a sample id",
+            url:"profile pic Url"
+        }
+    })
+
+    res.status(201).json({
+        success:true,
+        user
+    })
+})
