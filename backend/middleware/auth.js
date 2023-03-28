74a8410783e73ad5f5f6e30651184722947e8fdf
Rajugupta123 <raju.kgupta77@gmail.com>
2023-03-28 16:52:26 +0545

    admin auth added

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -17,4 +17,17 @@ exports.isAuthenticatedUser = catchAsyncErrors(async(req,res,next)=>{
     next();
 })
 
+exports.authorizedRoles = (...roles)=>{
+
+    return (req,res,next)=>{
+
+        if(!roles.includes(req.user.role)){
+            return next( new ErrorHandler(`Role: ${req.user.role} is not allowed to access resources`,403))
+        }
+        next();
+    }
+
+    
+}
+
 //module.exports = isAuthenticatedUser;
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const { getAllProducts,createProduct,updateProduct,deleteProduct,getSingleProduct } = require("../controllers/productController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+const { isAuthenticatedUser,authorizedRoles } = require("../middleware/auth");
 
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser,createProduct);
 
-router.route("/product/:id")
-.put(isAuthenticatedUser,updateProduct)
-.delete(isAuthenticatedUser,deleteProduct)
+router
+.route("/product/new")
+.post(isAuthenticatedUser, authorizedRoles("admin"),createProduct);
+
+router
+.route("/product/:id")
+.put(isAuthenticatedUser, authorizedRoles("admin"),updateProduct)
+.delete(isAuthenticatedUser, authorizedRoles("admin"),deleteProduct)
 .get(getSingleProduct)
 
 
