6b44f6269834355a4d1542c5a93279298e9496ce
Rajugupta123 <raju.kgupta77@gmail.com>
2023-03-29 21:41:14 +0545

    jwt auth added

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,6 +12,26 @@ module.exports = (err,req,res,next)=>{
         err = new ErrorHandler(message,400); 
     }
 
+    //Mongoose duplicate Key Error.
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message,400)
+
+    }
+
+    //Wrong JWT error.
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json web token is invalid, Try Again`;
+        err = new ErrorHandler(message,400); 
+    }
+    
+    // JWT Expire Error.
+    if(err.name === "TokenExpiredError"){
+        const message = `Json Web Token is Expired, Try Again`;
+        err = new ErrorHandler(message,400); 
+    }
+
+
 
     res.status(err.statusCode).json({
         success:false,
