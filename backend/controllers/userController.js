3e355a7bab213add7eaf8f9325791863bed216ac
Rajugupta123 <raju.kgupta77@gmail.com>
2023-03-29 18:30:16 +0545

    reset password nodemailer

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail")
-
+const crypto = require("crypto")
 
 
 //Register A User.
@@ -116,6 +116,39 @@ exports.forgotPassword = catchAsyncErrors(async(req,res,next)=>{
         return next(new ErrorHandler(error.message,500))
 
     }
-
-
 })
+
+//Reset Password
+exports.resetPassword = catchAsyncErrors(async(req,res,next)=>{
+
+    //creating token hash
+    const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex")
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: {$gt: Date.now()}  
+        
+    })
+
+    if(!user){
+        return next(new ErrorHandler("Reset Password Token is invalid or expired",400))
+    }
+
+    //confirm password
+    if(req.body.password !== req.body.confirmPassword){
+        return next(new ErrorHandler("Password doesnot match",400))
+    }
+
+    //if valid user and valid token
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save()
+
+    //redirected to login
+    sendToken(user,200,res)
+}) 
