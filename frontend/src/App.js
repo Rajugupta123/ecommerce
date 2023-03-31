9b45f3768120c378a95d201ac423505661f0a2c9
Rajugupta123 <raju.kgupta77@gmail.com>
2023-03-31 14:38:46 +0545

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import Header from "./component/layout/Header/Header"
 import Footer from "./component/layout/Footer/Footer"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "./component/Home/Home"
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h2 style={{textAlign:'center'}}>Welcome to Online Shopping</h2>
+      
       <Header/>
+      {/* <h2 style={{textAlign:'center'}}>Welcome to Online Shopping</h2> */}
+      <Router>
+        <Routes><Route path="/" element={<Home/>} /></Routes>
+          
+      </Router>
+      
       <Footer/>
     </div>
   );
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react'
+import {FaLongArrowAltDown} from "react-icons/fa"
+import Product from "./Product"
+import "./Home.css"
+
+
+const product = {
+    name:"T -shirt",
+    images:[{url:"https://rukminim1.flixcart.com/image/612/612/xif0q/t-shirt/y/t/k/xxs-t653-cgblwh-eyebogler-original-imaghyjv7kppbqxb.jpeg?q=70"}],
+    price: " Rs 2500",
+    _id:" shyam"
+}
+
+
+const Home = () => {
+  return (
+    <Fragment>
+        <div className='banner'>
+            <p>Welcome to Ecommerce</p>
+            <h1>Find Amazing Products Below</h1>
+
+            <a href='#container'>
+                <button>
+                    Scroll &nbsp;&nbsp;<FaLongArrowAltDown/>
+                </button>
+
+            </a>
+
+        </div>
+        <h2 className='homeHeading'>Featured Products</h2>
+        <div className='container' id='container'>
+            <Product product={product} />
+        </div>
+
+    </Fragment>
+  )
+}
+
+export default Home
